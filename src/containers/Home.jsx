9f9ed44920367b3297fe37679bb3f4378415925d
BenCutlerAmes <93072051+BenCutlerAmes@users.noreply.github.com>
2022-10-06 13:47:00 +0100

    construction image added

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import construction from '../images/construction_background.jpeg'
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
         <h1> Ben Cutler-Ames</h1>
         <h3>Software Developer</h3>
         <p> This site is currently under construction. </p>
+        <img id="construction-image" src={construction} alt="construction site" />
       </div>
     </>
   );
